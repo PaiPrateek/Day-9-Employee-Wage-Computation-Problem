09150d3fe9b8f4723f60cc9454d80c19f0b86550
Prateek Pai <paiprateek123@gmail.com>
2022-06-16 22:19:13 +0530

    Compute Employee Wage for Multiple Companies

--- EmployeeWageComputationProblem/EmpWage.cs
+++ EmployeeWageComputationProblem/EmpWage.cs
@@ -8,9 +8,63 @@ namespace EmployeeWageComputationProblem
 {
     public class EmpWage
     {
-        public static void CheckEmpWage()
+        public static void CheckEmpWage( string Company, int EmpWagePerHour, int WorkingDaysPerMonth, int WorkingHourPerMonth)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Problem");
+            int FullTime = 1;
+            int Absent = 0;
+            int PartTime = 2;
+            int EmpHrs = 0;
+            int EmpWage = 0;
+            int TotaklEmpWage = 0;
+            int TotalWorkingDays = 0;
+            int TotalWorkinhHours = 0;
+            int FullDay = 0, AbsentDay = 0, PartDay = 0;
+            Console.WriteLine("Company Name:"+ Company);
+            Console.WriteLine("...............................");
+            Console.WriteLine("...............................");
+            while (TotalWorkinhHours <= WorkingHourPerMonth && TotalWorkingDays < WorkingDaysPerMonth)
+            {
+                Random random = new Random();
+                int Attendence = random.Next(0, 3);
+                switch (Attendence)
+                {
+                    case 0:
+                        Console.WriteLine("Employee is Absent");
+                        EmpHrs = 0;
+                        AbsentDay++;
+                        break;
+                    case 1:
+                        Console.WriteLine("Employee is Present (Full Time)");
+                        EmpHrs = 8;
+                        FullDay++;
+                        break;
+                    case 2:
+                        Console.WriteLine("Employee is Present (Part Time) ");
+                        EmpHrs = 4;
+                        PartDay++;
+                        break;
+                }
+                Console.WriteLine("...............................");
+                EmpWage = EmpHrs * EmpWagePerHour;
+                TotalWorkinhHours += EmpHrs;
+                TotalWorkingDays++;
+                TotaklEmpWage += EmpWage;
+                Console.WriteLine("Employee Daily Wage is : " + EmpWage);
+
+            }
+            Console.WriteLine("...............................");
+            Console.WriteLine("...............................");
+            Console.WriteLine("Working Day is :" + TotalWorkingDays);
+            Console.WriteLine("Working Hours is :" + TotalWorkinhHours);
+            Console.WriteLine("...............................");
+            Console.WriteLine("...............................");
+            Console.WriteLine("Full Day Presence is :" + FullDay);
+            Console.WriteLine("Part Time Presnece is : " + PartDay);
+            Console.WriteLine("Absent is :" + AbsentDay);
+            Console.WriteLine("Total Employee Wage is :" + TotaklEmpWage);
+            Console.WriteLine("...............................");
+            Console.WriteLine("...............................");
+
         }
     }
 }
--- EmployeeWageComputationProblem/Program.cs
+++ EmployeeWageComputationProblem/Program.cs
@@ -2,11 +2,12 @@
 
 namespace EmployeeWageComputationProblem
 {
-    internal class Program
+    public  class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            EmpWage.CheckEmpWage();
+            EmpWage.CheckEmpWage("Infosys", 100, 20, 150);
+            EmpWage.CheckEmpWage("TATA", 80, 22, 100);
         }
     }
 }
