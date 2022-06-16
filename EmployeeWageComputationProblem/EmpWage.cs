cf39ae654b574f723ea559bc4e63a84a31de223e
Prateek Pai <paiprateek123@gmail.com>
2022-06-16 22:09:19 +0530

    Welcome to Employee Computation Problem

---
+++ EmployeeWageComputationProblem/EmpWage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputationProblem
+{
+    public class EmpWage
+    {
+        public static void CheckEmpWage()
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Problem");
+        }
+    }
+}
---
+++ EmployeeWageComputationProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWageComputationProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWage.CheckEmpWage();
+        }
+    }
+}
